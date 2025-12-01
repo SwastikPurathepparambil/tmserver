c9f98eed6ac1d9d3f69c949eedaf814abda0bc28
Ari Katsav <arikatsav@ucla.edu>
2025-11-30 22:08:14 -0800

    Add resume models.py

--- src/tmserver/models.py
+++ src/tmserver/models.py
@@ -1,8 +1,17 @@
 from datetime import datetime
 from pydantic import BaseModel
-
+from typing import Dict, Any, Optional
 class UserResponse(BaseModel):
     id: str
     email: str
     created_at: datetime
     last_login_at: datetime
+
+
+class CreateResume(BaseModel):
+    target_role: str
+    content: Dict[str, Any]
+
+class UpdateResume(BaseModel):
+    target_role: Optional[str] = None
+    content: Optional[Dict[str, Any]] = None
